6d5d73d31cb3eb179271938042e80261280c25b6
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-05-29 23:30:37 +0200

    Update UI and some logic

--- MusicVibes/MainWindow.xaml.cs
+++ MusicVibes/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using System.Windows;
 using System.Windows.Input;
 
 namespace MusicVibes
@@ -13,6 +14,22 @@ namespace MusicVibes
         private void Border_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left) this.DragMove();
+        }
+
+        private void QuitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void OpenFiles_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "All Media Files|*.wav;*.mp3;*.WAV;*.MP3;";
+            openFileDialog.Multiselect = true;
+            if(openFileDialog.ShowDialog() == true)
+            {
+
+            }
         }
     }
 }
