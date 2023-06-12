6c8cccfd346f29ce4c50765ec34ee950e495125e
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-06-13 01:22:41 +0200

    Update Logic of AudiPlayer

--- MusicVibes/AudioPlayer.cs
+++ MusicVibes/AudioPlayer.cs
@@ -5,18 +5,21 @@ namespace MusicVibes.Models
     class AudioPlayer
     {
         private WindowsMediaPlayer WMP;
-        public AudioPlayer() => WMP = new WindowsMediaPlayer();
+        public AudioPlayer()
+        {
+            WMP = new WindowsMediaPlayer();
+            WMP.settings.volume = 50;
+        }
         public void Load(string path)
         {
-            Stop();
-            WMP.settings.volume = 10;
+            WMP.controls.stop();
             WMP.URL = path;
             WMP.controls.play();
         }
         public void Pause() => WMP.controls.pause();
         public void Play() => WMP.controls.play();
-        public void Stop() => WMP.controls.stop();
-        public void ChangeVolume(short value) => WMP.settings.volume = value;
+        public void ChangeVolume(short value) => WMP.settings.volume = value; // to
         public void SkipTrack(int count) => WMP.controls.currentPosition += count;
+        public bool IsJustStarted() => WMP.controls.currentPosition < 2.0d ? true : false;
     }
 }
