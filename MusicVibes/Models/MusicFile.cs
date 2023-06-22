19c166a07d9bf8e1e6a22583d372dade8bcb3213
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-06-22 19:49:13 +0200

    Correct Model

--- MusicVibes/Models/MusicFile.cs
+++ MusicVibes/Models/MusicFile.cs
@@ -2,18 +2,17 @@
 
 public class MusicFile
 {
-    public int FileId { get; set; } = -1;
-    public string FilePath { get; set; } = string.Empty;
-    public string FileName { get; set; } = string.Empty;
-    public string FileExtension { get; set; } = string.Empty;
-    public string FileDuration { get; set; } = string.Empty;
-
-    public MusicFile(int FileId, string FilePath, string FileName, string FileExtension, string FileDuration)
+    public int FileId { get; }
+    public string FilePath { get; }
+    public string FileName { get; }
+    public double FileDuration { get; }
+    public string FileDurationString { get; }
+    public MusicFile(int FileId, string FilePath, string FileName, double FileDuration, string FileDurationString)
     {
         this.FileId = FileId;
         this.FilePath = FilePath;
         this.FileName = FileName;
-        this.FileExtension = FileExtension;
         this.FileDuration = FileDuration;
+        this.FileDurationString = FileDurationString;
     }
 }
