552b4fe861b34f8a370c394fd493f89114c12941
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2024-03-22 19:31:49 +0100

    Update SettingsPage.xaml.cs

--- MusicVibes/Pages/SettingsPage.xaml.cs
+++ MusicVibes/Pages/SettingsPage.xaml.cs
@@ -1,28 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
+﻿#pragma warning disable 8602
+
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Collections.ObjectModel;
 
-namespace MusicVibes.Pages
+namespace MusicVibes.Pages;
+
+public partial class SettingsPage : Page
 {
-    /// <summary>
-    /// Logika interakcji dla klasy SettingsPage.xaml
-    /// </summary>
-    public partial class SettingsPage : Page
+    public ObservableCollection<string> themes { get; } = new ObservableCollection<string>();
+    public ObservableCollection<string> languages { get; } = new ObservableCollection<string>();
+    public string selectedTheme { get; private set; } = "Light";
+    public string selectedLanguage { get; private set; } = "English";
+
+    public SettingsPage(string theme, string language)
     {
-        public SettingsPage()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+        themes.Add("Light");
+        themes.Add("Dark");
+        themes.Add("Grey");
+        languages.Add("English");
+        languages.Add("French");
+        languages.Add("German");
+        languages.Add("Italian");
+        languages.Add("Japanese");
+        languages.Add("Polish");
+        languages.Add("Spanish");
+        if (themes.Contains(theme)) selectedTheme = theme;
+        if (languages.Contains(language)) selectedLanguage = language;
+        Application.Current.Resources.MergedDictionaries.First().Source =
+            new Uri($"pack://application:,,,/Themes/Colors/{selectedTheme}.xaml");
+        Application.Current.Resources.MergedDictionaries.Last().Source =
+            new Uri($"pack://application:,,,/Languages/{selectedLanguage}.xaml");
+    }
+
+    private void Theme_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        Application.Current.Resources.MergedDictionaries.First().Source =
+            new Uri($"pack://application:,,,/Themes/Colors/{themes[(sender as ListView).SelectedIndex]}.xaml");
+        selectedTheme = themes[(sender as ListView).SelectedIndex];
+    }
+
+    private void Language_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        Application.Current.Resources.MergedDictionaries.Last().Source =
+            new Uri($"pack://application:,,,/Languages/{languages[(sender as ListView).SelectedIndex]}.xaml");
+        selectedLanguage = languages[(sender as ListView).SelectedIndex];
     }
 }
