075900a25edd2a2a5c374c43ebaa3bf927e1b703
Patryk Wójtowicz <ultiprogames@gmail.com>
2024-04-07 17:10:50 +0200

    New focus Style and space handler

--- MusicVibes/UserControls/MusicList.xaml.cs
+++ MusicVibes/UserControls/MusicList.xaml.cs
@@ -1,4 +1,4 @@
-﻿#pragma warning disable CS8618
+﻿#pragma warning disable CS8602, CS8618
 
 using System.Windows;
 using System.Windows.Controls;
@@ -11,6 +11,8 @@ public partial class MusicList : UserControl
     public int? selectedMusic { get; private set; } = null;
     public delegate void OnMusicChange(object sender, RoutedEventArgs e, int id);
     public event OnMusicChange onMusicChange;
+    public delegate void OnFocusChange(object sender, KeyEventArgs e);
+    public event OnFocusChange onFocusChange;
 
     public MusicList() => InitializeComponent();
 
@@ -25,4 +27,8 @@ public partial class MusicList : UserControl
         ListView? listView = sender as ListView;
         if (listView != null) selectedMusic = listView.SelectedIndex;
     }
+
+    private void ListViewItem_SpacePreviewKeyDown(object sender, KeyEventArgs e) => onFocusChange(sender, e);
+
+    public void SetItemFocus(int id) => List.SelectedIndex = id;
 }
